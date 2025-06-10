c7c1d0edbbab365e33d7f492b1e03bb1c86b8f90
Mapakitus <mapakitus@gmail.com>
2025-06-10 11:48:11 +0200

    CREAR ENUM ConservationStatus

--- src/main/java/com/grajilla/aviberico/entities/Bird.java
+++ src/main/java/com/grajilla/aviberico/entities/Bird.java
@@ -1,17 +1,27 @@
 package com.grajilla.aviberico.entities;
 
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "birds")
 public class Bird {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false)
     private String commonName;
     private String species;
     private String genus;
     private String family;
+
+    @Column(name = "bird_order") // 'order' es una palabra reservada en SQL, por eso usamos 'bird_order'
     private String order;
+
+    @Column(length = 1000)
     private String description;
+
     private ConservationStatus conservationStatus (enum);
 
 
